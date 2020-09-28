17ce1cb02f50cfa2ab74642e7a5653a50c1dd4b1
mike connor <mike@rocketnumber9.org>
2020-09-28 13:46:04 -0500

    App - analytics and auth

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Auth from '@aws-amplify/auth';
+import Analytics from '@aws-amplify/analytics';
+import awsconfig from './aws-exports';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+Auth.configure(awsconfig);
+Analytics.configure(awsconfig);
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    Analytics.record('AWS Amplify Course Event');
+
+
+    Analytics.record({
+        name: 'Course',
+        attributes: { tech: 'AWS Amplify', author: 'Bear' }
+    });
+    Analytics.record({
+        name: 'Course',
+        attributes: { tech: 'AWS IAM', author: 'Bear' }
+    });
+    console.log("test");
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-intro">
+        </div>
+      </div>
+    );
+  }
 }
+
 
 export default App;
